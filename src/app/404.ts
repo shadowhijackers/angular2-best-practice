d2bef568623112def734563ef70dfb837eba0227
agiratech-nagaraj <nagaraj@agiratech.com>
2019-04-15 11:04:03 +0530

    index page redrection link added in 404 page

--- src/app/404.ts
+++ src/app/404.ts
@@ -3,7 +3,10 @@ import {APPName} from './config';
 
 const templateHTML = `
  <main class="sh-u-full-height">
-    <h1 style="position: absolute;right: 0;" class="sh-theme-primary sh-display-2 sh-u-text-align-right sh-u-p-8">{{ appName }}</h1>
+    <a routerLink="/"
+       style="position: absolute;right: 0;"
+       class="sh-theme-primary sh-display-2 sh-u-text-align-right sh-u-p-8"
+       >{{ appName }}</a>
     <article style="display: flex;flex-flow: column; justify-content: center; align-items: center; height: 100%">
           <h1 class="sh-theme-primary sh-display-4 sh-u-text-align-center sh-u-m-8">404</h1>
           <h2 class="sh-theme-accent sh-display-2  sh-u-text-align-center sh-u-m-8"> Page Not Found</h2>
