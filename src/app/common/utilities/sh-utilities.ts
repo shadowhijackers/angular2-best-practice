d94633bc3de4c75a34b3f2c489d42715fd6e5b60
agiratech-nagaraj <nagaraj@agiratech.com>
2019-04-15 11:33:57 +0530

    utilities added

--- src/app/common/utilities/sh-utilities.ts
+++ src/app/common/utilities/sh-utilities.ts
@@ -1,5 +1,27 @@
 export class ShUtilities {
+
   static isEmpty(data: any) {
     return !data;
   }
+
+  static isFunction(data) {
+    return typeof data === 'function';
+  }
+
+  static isNumber(data) {
+    return typeof data === 'number';
+  }
+
+  static isObject(data) {
+    return typeof data === 'object';
+  }
+
+  static isString(data) {
+    return typeof data === 'string';
+  }
+
+  static isArray(data) {
+    return Array.isArray(data);
+  }
+
 }
